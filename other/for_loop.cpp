ba62daee01bde0983c59585ce6f0251474536fca
Tigran Margaryan <tigranmargarian@outlook.com>
2022-12-19 16:52:39 +0400

    for loop prints

---
+++ other/for_loop.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+void printTri1(int n){
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=i; j++){
+            std::cout << '*';
+        }
+        std::cout << std::endl;
+    }
+}
+
+void printTri2(int n){
+    for (int i=n; i>0; i--){
+        for (int j=1; j<=i; j++){
+            std::cout << '*';
+        }
+        std::cout << std::endl;
+    }
+}
+
+void printTri3(int n){
+    for (int i=n; i>0; i--){
+
+        for (int j=1; j<=i-1; j++){
+            std::cout << ' ';
+        }
+
+        for (int j=1; j<=n-i+1; j++){
+            std::cout << '*';
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+void printTri4(int n){
+    for (int i=n; i>0; i--){
+
+        for (int j=0; j<=n-i-1; j++){
+            std::cout << ' ';
+        }
+
+        for (int j=0; j<=i-1; j++){
+            std::cout << '*';
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+void printTri5(int n){
+
+    int mid = n / 2;
+    for (int i=n; i>0; i--){
+
+        for (int j=1; j<=i-1; j++){
+            std::cout << ' ';
+        }
+
+        for (int j=1; j<=n-i+1; j++){
+            std::cout << '*';
+        }
+
+        for (int j=0; j<=n-i-1; j++){
+            std::cout << '*';
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+void printTri6(int n){
+
+    int mid = n / 2;
+    for (int i=1; i<=n; i++){
+
+        for (int j=1; j<=i-1; j++){
+            std::cout << ' ';
+        }
+
+        for (int j=1; j<=n-i+1; j++){
+            std::cout << '*';
+        }
+
+        for (int j=0; j<=n-i-1; j++){
+            std::cout << '*';
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+
+int main(){
+    int n = 5;
+    printTri6(n);
+}
