61fd89eba6c8531e535aaf41a18eb0973e31a5b5
Tigran Margaryan <tigranmargarian@outlook.com>
2022-12-19 17:45:47 +0400

    for loops in C++

--- other/for_loop.cpp
+++ other/for_loop.cpp
@@ -21,7 +21,7 @@ void printTri2(int n){
 void printTri3(int n){
     for (int i=n; i>0; i--){
 
-        for (int j=1; j<=i-1; j++){
+        for (int j=1; j<i; j++){
             std::cout << ' ';
         }
 
@@ -36,11 +36,11 @@ void printTri3(int n){
 void printTri4(int n){
     for (int i=n; i>0; i--){
 
-        for (int j=0; j<=n-i-1; j++){
+        for (int j=0; j<n-i; j++){
             std::cout << ' ';
         }
 
-        for (int j=0; j<=i-1; j++){
+        for (int j=0; j<i; j++){
             std::cout << '*';
         }
 
@@ -74,7 +74,7 @@ void printTri6(int n){
     int mid = n / 2;
     for (int i=1; i<=n; i++){
 
-        for (int j=1; j<=i-1; j++){
+        for (int j=1; j<i; j++){
             std::cout << ' ';
         }
 
@@ -82,7 +82,7 @@ void printTri6(int n){
             std::cout << '*';
         }
 
-        for (int j=0; j<=n-i-1; j++){
+        for (int j=0; j<n-i; j++){
             std::cout << '*';
         }
 
@@ -93,5 +93,22 @@ void printTri6(int n){
 
 int main(){
     int n = 5;
+
+    printTri1(n);
+    std::cout << std::endl;
+
+    printTri2(n);
+    std::cout << std::endl;
+
+    printTri3(n);
+    std::cout << std::endl;
+
+    printTri4(n);
+    std::cout << std::endl;
+
+    printTri5(n);
+    std::cout << std::endl;
+
     printTri6(n);
+    std::cout << std::endl;
 }
