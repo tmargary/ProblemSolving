ec4dab1ce04304c6615c3b01c9d0f8fc15683851
Tigran Margaryan <tigranmargarian@outlook.com>
2023-01-27 15:55:48 +0400

    Goal Parser Interpretation

---
+++ leetcode/easy/problem_1678.cpp
@@ -0,0 +1,52 @@
+/*
+1678. Goal Parser Interpretation
+
+You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+Given the string command, return the Goal Parser's interpretation of command.
+
+
+
+Example 1:
+
+Input: command = "G()(al)"
+Output: "Goal"
+Explanation: The Goal Parser interprets the command as follows:
+G -> G
+() -> o
+(al) -> al
+The final concatenated result is "Goal".
+
+Example 2:
+
+Input: command = "G()()()()(al)"
+Output: "Gooooal"
+
+Example 3:
+
+Input: command = "(al)G(al)()()G"
+Output: "alGalooG"
+
+*/
+
+class Solution {
+public:
+    string interpret(string command) {
+        string result;
+        for (int i{}; i < command.size(); ++i){
+            char this_ = command[i];
+            char next_ = command[i+1];
+
+            if (this_ == 'a' || this_ == 'l' || this_ == ')'){
+                continue;
+            } else if (this_ == 'G'){
+                result += 'G';
+            } else if (this_ == '(' && next_ == 'a') {
+                result += "al";
+            } else if (this_ == '(') {
+                result += "o";
+            }
+        }
+    return result;
+    }
+};
