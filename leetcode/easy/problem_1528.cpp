a8c29bff872ae3db2e00ba3ab3dcd627e52c8eed
Tigran Margaryan <tigranmargarian@outlook.com>
2023-01-27 16:21:05 +0400

    Shuffle String

---
+++ leetcode/easy/problem_1528.cpp
@@ -0,0 +1,34 @@
+/*
+1528. Shuffle String
+
+You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+
+
+
+Example 1:
+
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+Example 2:
+
+Input: s = "abc", indices = [0,1,2]
+Output: "abc"
+Explanation: After shuffling, each character remains in its position.
+
+*/
+
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string result = s;
+
+        for (int i{}; i < s.size(); ++i){
+            result[indices[i]] = s[i];
+        }
+    return result;
+    }
+};
