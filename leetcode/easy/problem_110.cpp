be1d1aca4c90f80142b6bbe481ea698a1f07befc
Tigran Margaryan <tigranmargarian@outlook.com>
2023-02-22 12:24:06 +0400

    Validate Binary Search Tree

--- leetcode/easy/problem_110.cpp
+++ leetcode/easy/problem_110.cpp
@@ -1,3 +1,51 @@
 /*
+110. Balanced Binary Tree
 
+Given a binary tree, determine if it is
+height-balanced.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+Example 2:
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+Example 3:
+Input: root = []
+Output: true
 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int getHeight(TreeNode* node) {
+        if (node == nullptr) {
+            return 0;
+        }
+        int leftHeight = getHeight(node->left);
+        int rightHeight = getHeight(node->right);
+        return max(leftHeight, rightHeight) + 1;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        if (root == nullptr) {
+            return true;
+        }
+        int leftHeight = getHeight(root->left);
+        int rightHeight = getHeight(root->right);
+        return abs(leftHeight - rightHeight) <= 1
+            && isBalanced(root->left)
+            && isBalanced(root->right);
+    }
+};
